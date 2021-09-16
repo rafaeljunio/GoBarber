9405bddc0d17027b6257ee7ddea0d354e6aa43e3
Rafael Junio <rafaeljunio7@gmail.com>
2021-09-16 10:27:05 -0300

    first commit

---
+++ Server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'aba903c5329f8ae23b051b29bd8d9687',
+    expiresIn: '1d',
+  },
+};
---
+++ Server/src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,61 @@
+/* eslint-disable prettier/prettier */
+
+import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUserRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) { }
+
+  public async execute({ email, password }: IRequest): Promise<Response> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    // user.password - Senha criptografada
+    // password - Senha não criptografada
+
+    const passwordMatched = await this.hashProvider.compareHash(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
